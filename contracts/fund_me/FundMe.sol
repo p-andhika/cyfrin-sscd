6d20d6563f00dde393319fe80865e6f979a200ef
p-andhika <p.andhika@op.iitg.ac.in>
2025-01-27 14:06:16 +0700

    set minimum ammount

--- contracts/fund_me/FundMe.sol
+++ contracts/fund_me/FundMe.sol
@@ -2,9 +2,11 @@
 pragma solidity 0.8.24;
 
 contract FundMe {
+    uint256 public minimumUsd = 5;
+
     // Get funds from users
     function fund() public payable {
-        require(msg.value > 1e18, "Didn't send enough ETH"); // 1e18 = 1 ETH = 1 * 10 ** 18
+        require(msg.value >= minimumUsd, "Didn't send enough ETH"); // 1e18 = 1 ETH = 1 * 10 ** 18
     }
 
     // Withdraw funds
