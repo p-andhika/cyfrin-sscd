df39149e5f62ef8df22cdf124942ed766125b178
p-andhika <p.andhika@op.iitg.ac.in>
2025-01-27 15:49:32 +0700

    import library

--- contracts/fund_me/FundMe.sol
+++ contracts/fund_me/FundMe.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     uint256 public minimumUsd = 5;
 
@@ -9,6 +11,14 @@ contract FundMe {
         require(msg.value >= minimumUsd, "Didn't send enough ETH"); // 1e18 = 1 ETH = 1 * 10 ** 18
     }
 
+    function getPrice() public {
+        // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI 
+    }
+
+    function getConversionRate() public {
+
+    }
     // Withdraw funds
     // function withdraw() public {
 
